167e16dc9aec164601d919732a499e0fe66fb869
Sai Reddy <saikumarreddypalakolanu6@gmail.com>
2025-07-14 10:54:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://onlinequizapp-frontend.onrender.com/',
   credentials: true
 }));
 app.use(express.json());
